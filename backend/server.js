b9b9000ecdfd3b716911b999d49885ea76deb6ed
rwan <rwanmatter1@gmail.com>
2024-10-14 22:51:01 +0300

    rwan 6:09:27

---
+++ backend/server.js
@@ -0,0 +1,6 @@
+//rwan 5:56:17
+import userRouter from "./routes/userRoute";
+
+//api endpoints rwan 5:55:53, ahmed will create "app" so it may not work before that
+app.use('/api/user',userRouter)
+
