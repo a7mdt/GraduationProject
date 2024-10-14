f1821ebf20840e5ae563a35d8f1bdd7efcc508c2
a7mdt <ahmedelabd.one@gmail.com>
2024-10-14 04:28:42 +0300

    ..

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,12 +3,12 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { ShopContext } from "./context/ShopContext.jsx";
+import ShopContextProvider from "./context/ShopContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <ShopContext.Provider>
+    <ShopContextProvider>
       <App />
-    </ShopContext.Provider>
+    </ShopContextProvider>
   </BrowserRouter>
 );
