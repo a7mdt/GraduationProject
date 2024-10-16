324fa06c37bb3cd31205423567f0092cec594af6
rwan <rwanmatter1@gmail.com>
2024-10-16 19:37:27 +0300

    half collection, little cart

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+//uncomment it work but not its time--------------------------------------------------------
+// import {useContext, useEffect} from "react"
+// import {ShopContext} from '../context/ShopContext'
+import Title from '../components/Title'
 
 const Cart = () => {
+
+
+//uncomment it work but not its time--------------------------------------------------------
+  // const{products, currency, cartItems}= useContext(ShopContext);
+
+  // const [cartData,setCartData]=useContext([]);
+
+  // useEffect(()=>{
+    
+  //   const tempData=[];
+  //   for(const items in cartItems){
+  //     for(const item in cartItems[items]){
+  //       if(cartItems[items][item]>0){
+  //         tempData.push({
+  //           _id: items,
+  //           size: item,
+  //           quantity:cartItems[items][item]
+  //         })
+  //       }
+  //     }
+  //   }
+  //   setCartData(tempData);
+  // },[cartItems])
+
   return (
-    <div>Cart</div>
+    <div className='border-t pt-14'>
+      <div className='text-2xl mb-3'>
+      <Title text1="YOUR" text2="CART" />
+      </div>
+      </div>
   )
 }
 
