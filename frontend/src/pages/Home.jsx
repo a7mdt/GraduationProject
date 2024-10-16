1bfa075c3f1e3fc164b026c3162c84671143d100
mennasalem <mennasalem51@gmail.com>
2024-10-17 01:42:31 +0300

    finish home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection'
-// import ProductItem from '../components/ProductItem'
+import BestSeller from '../components/BestSeller'
+import OurPolicy from '../components/OurPolicy'
+import NewsLetterBox from '../components/NewsLetterBox'
+
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <LatestCollection />
-      {/* <ProductItem /> */}
+      <BestSeller />
+      <OurPolicy/>
+      <NewsLetterBox/>
     </div>
   )
 }
