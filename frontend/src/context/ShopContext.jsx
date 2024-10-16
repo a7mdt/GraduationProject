99e76e95701ef9584844a9546f70c41caa5c59d5
mennasalem <mennasalem51@gmail.com>
2024-10-16 22:20:28 +0300

    aaaaaaaah yana

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -17,3 +17,35 @@ const ShopContextProvider = (props) => {
 };
 
 export default ShopContextProvider;
+
+
+// import { createContext } from "react";
+// import { products } from "../assets/assets";
+// import PropTypes from 'prop-types'; // Import PropTypes
+
+// export const ShopContext = createContext();
+
+// const ShopContextProvider = ({ children }) => {
+//   const currency = "$";
+//   const delivery_fee = 10;
+
+//   const value = {
+//     products,
+//     currency,
+//     delivery_fee,
+//   };
+
+//   return (
+//     <ShopContext.Provider value={value}>
+//       {children}
+//     </ShopContext.Provider>
+//   );
+// };
+
+// // PropTypes validation
+// ShopContextProvider.propTypes = {
+//   children: PropTypes.node.isRequired, // children must be a valid React node and required
+// };
+
+// export default ShopContextProvider;
+
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Hero from '../components/Hero'
-import LatestCollection from'../components/LatestCollection'
+import LatestCollection from '../components/LatestCollection'
+// import ProductItem from '../components/ProductItem'
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <LatestCollection />
-
+      {/* <ProductItem /> */}
     </div>
   )
 }
