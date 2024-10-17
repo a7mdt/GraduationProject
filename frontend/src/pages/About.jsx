a8aeb87073cbc45b3cd73e3bbd518dd0cce0b97f
rwan <rwanmatter1@gmail.com>
2024-10-17 12:51:25 +0300

    About&Contact Us end

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+// import React from 'react'
+import { assets } from "../assets/assets";
+import Title from "../components/Title";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div>
+      <div className="text-2xl text-center pt-8 border-t">
+        <Title text1={"ABOUT"} text2={" US"} />
+      </div>
 
-export default About
+      <div className="my-10 flex flex-col md:flex-row gap-16">
+        <img
+          className="w-full md:max-w-[450px]"
+          src={assets.about_img}
+          alt=""
+        />
+        <div className="flex flex-col justify-center gap-6 md:w-2/4 text-gray-600">
+          <p>about text slkjdfsfklsjfldksfjsflsjflsfjfjsfjfdslfk</p>
+          <p>another about text fdgkdfjdlkjlkfjskfjsfklsjflsknsdkfjskfjsf</p>
+          <b className="text-gray-800">Our Mission</b>
+          <p>dummy text jsdfoisjdofijdsiofjsfjsif</p>
+        </div>
+      </div>
+
+      <div className="text-xl py-4">
+        <Title text1={"WHY"} text2={" CHOOSE US"} />
+      </div>
+
+      <div className="flex flex-col md:flex-row text-sm mb-">
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex-col gap-5">
+          <b>Quality Assurance:</b>
+          <p className="text-gray-600">dummy text</p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex-col gap-5">
+          <b>Convenience:</b>
+          <p className="text-gray-600">dummy text</p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex-col gap-5">
+          <b>Exceptional Customer Service:</b>
+          <p className="text-gray-600">dummy text</p>
+        </div>
+      </div>
+      {/* not its time i think uncomment in the very end */}
+      {/* <NewsLetterBox/> */}
+    </div>
+  );
+};
+
+export default About;
--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+// import React from 'react'
+import Title from "../components/Title";
+import { assets } from "../assets/assets";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <div>
+      <div className="text-center text-2xl pt-10 border-t">
+        <Title text1={"CONTACT"} text2={" US"} />
+      </div>
 
-export default Contact
+      <div className="my-10 flex flex-col justify-center md:flex-row gap-10 mb-28">
+        <img
+          className="w-full md:max-w-[480px]"
+          src={assets.contact_img}
+          alt=""
+        />
+        <div className="flex flex-col justify-center items-start gap-6">
+          <p className="font-semibold text-gray-600">Our Store</p>
+          <p className="text-gray-500">dummy address</p>
+          <p className="text-gray-500">Tel: dummy phone num</p>
+          <p className="font-semibold text-gray-600">Careers at Forever</p>
+          <p className="text-gray-500">
+            learn more about our teams and job openings
+          </p>
+          <p className="border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500">
+            Explore Jobs
+          </p>
+        </div>
+      </div>
+      {/* not its time i think uncomment in the very end */}
+      {/* <NewsLetterBox/> */}
+    </div>
+  );
+};
+
+export default Contact;
