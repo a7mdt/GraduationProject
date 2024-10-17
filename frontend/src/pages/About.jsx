ddcd2ce48fc6e279a29c491c578eec5c3e29f3fa
rwan <rwanmatter1@gmail.com>
2024-10-17 15:32:42 +0300

    ..

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -27,7 +27,7 @@ const About = () => {
         <Title text1={"WHY"} text2={" CHOOSE US"} />
       </div>
 
-      <div className="flex flex-col md:flex-row text-sm mb-">
+      <div className="flex flex-col md:flex-row text-sm mb-20">
         <div className="border px-10 md:px-16 py-8 sm:py-20 flex-col gap-5">
           <b>Quality Assurance:</b>
           <p className="text-gray-600">dummy text</p>
