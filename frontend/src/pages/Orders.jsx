69bd74d3cfdbf61347728272cfa67470e9e2b663
afafibrahim3 <afafibrahim954@gmail.com>
2024-10-20 20:29:12 +0300

    Afaf

--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -21,7 +21,7 @@ const Orders = () => {
       <div>
         {
           products.slice(1,4).map((item,index) => (
-            <div key={index} className='py-4 border-t border-b text-gray-700 flex-col md:flex-row md:item-center md:justify-between gap-4'>
+            <div key={index} className='py-4 border-t border-b text-gray-700 flex-col md:flex-row md:items-center md:justify-between gap-4'>
               <div className='flex items-start gap-6 text-sm'>
                 <img className='w-16 sm:w-20' src={item.image[0]} alt=""/>
                 <div>
@@ -31,15 +31,15 @@ const Orders = () => {
                     <p>Quantity: 1</p>
                     <p>Size: M</p>
                   </div>
-                  <p className='mt-2'>Data<span className='text-gray-400'>25, Jui, 2024</span></p>
+                  <p className='mt-2'>Data:<span className='text-gray-400'>25, Jui, 2024</span></p>
                 </div>
               </div>
               <div className='md:w-1/2 flex justify-between'>
                 <div className='flex items-center gap-2'>
                   <p className='min-w-2 h-2 rounded-full bg-green-500'></p>
-                  <p className='text-sm md:text-base'> Ready to ship</p>
+                  <p className='text-sm md:text-base'>Ready to ship</p>
                 </div>
-                <button className='border px-4 py-2 text-sm font-medium rounded-sm'></button>
+                <button className='border px-4 py-2 text-sm font-medium rounded-sm'>Track Order</button>
               </div>
             </div>
           ))
